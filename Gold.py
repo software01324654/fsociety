3d22c58cfd094e3f5d6c2ec782ce325f7bf7662e
XexinoX-F <73955457+riz4d@users.noreply.github.com>
2021-08-22 17:00:31 +0530

    Create Gold.py

---
+++ Gold.py
@@ -0,0 +1,24 @@
+
+[
+  {
+    "login": "RIZADMUHAMMED",
+    "id": 16539311,
+    "node_id": "MDQ6VXNlcjE2NTM5MzEx",
+    "avatar_url": "https://avatars.githubusercontent.com/u/16539311?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/riz4d",
+    "html_url": "https://github.com/riz4d",
+    "followers_url": "https://api.github.com/users/riz4d/followers",
+    "following_url": "https://api.github.com/users/riz4d/following{/other_user}",
+    "gists_url": "https://api.github.com/users/riz4d/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/riz4d/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/riz4d/subscriptions",
+    "organizations_url": "https://api.github.com/users/riz4d/orgs",
+    "repos_url": "https://api.github.com/users/riz4d/repos",
+    "events_url": "https://api.github.com/users/riz4d/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/riz4d/received_events",
+    "type": "User",
+    "site_admin": false,
+    "contributions": 106
+  }
+]
